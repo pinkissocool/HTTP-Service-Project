0f145718f16bd14819ec9b5d598936557b75e619
hungryTechie <iirimina@schools.nyc.gov>
2022-08-26 14:11:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+/*Tools: code editor, browser, command line utility, 
+application and server utility, API platform
+*/
 
 //=========== ROUTES FOR HTTP GET REQUESTS ==========
+
+
+
+
+//=========== ROUTES FOR HTTP POST REQUESTS ==========
+
+
+
+
+//=========== ROUTES FOR HTTP PUT REQUESTS ==========
+
+
+
+
+//=========== ROUTES FOR HTTP DELETE REQUESTS ==========
 
