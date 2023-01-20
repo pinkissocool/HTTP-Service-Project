d6105a276a30bf9b96ddd797765680ad0906ed8b
pinkissocool <99200272+pinkissocool@users.noreply.github.com>
2023-01-20 14:04:45 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /*Tools: code editor, browser, command line utility, 
 application and server utility, API platform
 */
+const express = require(express);
+const app = express();
+app.use(express.JSON);
+
+const music = [
+    {
+
+]
+
 
 //=========== ROUTES FOR HTTP GET REQUESTS ==========
 
